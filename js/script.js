a6936cb98a2de3c5e38e79e1694df90d285b4a91
Jodecir Neto <jodecirneto@gmail.com>
2020-12-14 13:31:53 -0300

    check winner

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
-let player = null;
+let player, winner = null;
 let selectedPlayer = document.getElementById('selectedPlayer');
+let selectedWinner = document.getElementById('selectedWinner');
 
 changePlayer('X');
 
 function selectBox(id) {
+  if (winner !== null) {
+    return;
+  }
+
   let box = document.getElementById(id);
 
-  if (box.innerHTML !== '-') {
+  if (box.innerHTML !== '!') {
     return;
   }
 
@@ -19,9 +24,89 @@ function selectBox(id) {
     player = 'X';
   }
   changePlayer(player);
+  checkWinner();
 }
 
-function changePlayer(valor) {
-  player = valor;
+function changePlayer(value) {
+  player = value;
   selectedPlayer.innerHTML = player;
+}
+
+function checkWinner() {
+  let box1 = document.getElementById(1);
+  let box2 = document.getElementById(2);
+  let box3 = document.getElementById(3);
+  let box4 = document.getElementById(4);
+  let box5 = document.getElementById(5);
+  let box6 = document.getElementById(6);
+  let box7 = document.getElementById(7);
+  let box8 = document.getElementById(8);
+  let box9 = document.getElementById(9);
+
+  if (checkOrder(box1,box2,box3)) {
+    changeBoxColor(box1,box2,box3);
+    changeWinner(box1);
+    return;
+  }
+  
+  if (checkOrder(box4,box5,box6)) {
+    changeBoxColor(box4,box5,box6);
+    changeWinner(box4);
+    return;
+  }
+  
+  if (checkOrder(box7,box8,box9)) {
+    changeBoxColor(box7,box8,box9);
+    changeWinner(box7);
+    return;
+  }
+  
+  if (checkOrder(box1,box4,box7)) {
+    changeBoxColor(box1,box4,box7);
+    changeWinner(box1);
+    return;
+  }
+
+  if (checkOrder(box2,box5,box8)) {
+    changeBoxColor(box2,box5,box8);
+    changeWinner(box2);
+    return;
+  }
+  
+  if (checkOrder(box3,box6,box9)) {
+    changeBoxColor(box3,box6,box9);
+    changeWinner(box3);
+    return;
+  }
+  
+  if (checkOrder(box1,box5,box9)) {
+    changeBoxColor(box1,box5,box9);
+    changeWinner(box1);
+    return;
+  }
+  
+  if (checkOrder(box3,box5,box7)) {
+    changeBoxColor(box3,box5,box7);
+    changeWinner(box3);
+  }
+}
+
+function changeWinner(box) {
+  winner = box.innerHTML;
+  selectedWinner.innerHTML = winner;
+}
+
+function changeBoxColor(box1,box2,box3) {
+  box1.style.background = '#0f0';
+  box2.style.background = '#0f0';
+  box3.style.background = '#0f0';
+}
+
+function checkOrder(box1,box2,box3) {
+  let equal = false;
+  
+  if (box1.innerHTML !== '!' && box1.innerHTML === box2.innerHTML && box2.innerHTML === box3.innerHTML) {
+    equal = true;
+  }
+  return equal;
 }
